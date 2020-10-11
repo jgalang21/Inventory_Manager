294048c7dcb36ee222d5052456589533a4c844b1
Jeremy Galang <jjgalang@iastate.edu>
2020-10-11 14:10:39 -0500

    login + homepage html added

---
+++ src/javascript/VerifyLogin.js
@@ -0,0 +1,9 @@
+// TODO THIS CLASS WILL CONTAIN MORE DATABASE LOGIC SOON
+
+function redirect(){
+  		//wait 2 seconds prior before going to the next page.
+        setTimeout(function(){
+            window.location.href = 'inventoryManager.html';}
+            , 2000);
+    
+}
