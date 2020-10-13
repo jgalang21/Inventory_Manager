682a477fe3da2fde2ed72d1f4127da0c231c9f5e
Jeremy <97.jeremy@gmail.com>
2020-10-13 16:32:42 -0500

    database changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# G47
+# How to run
 
+Simply navigate to /src/html/ and open the homeLogin.html file using a browser!
--- src/javascript/VerifyLogin.js
+++ src/javascript/VerifyLogin.js
@@ -5,5 +5,22 @@ function redirect(){
         setTimeout(function(){
             window.location.href = 'inventoryManager.html';}
             , 2000);
-    
+
+}
+
+function verify(){
+
+  var mysql = require('mysql');
+
+  var con = mysql.createConnection({
+    host: "coms-319-047.cs.iastate.edu",
+    user: "root",
+    password: "comsVM@319"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
 }
