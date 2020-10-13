b66d020c25fc6b4815acdd22fc10f0593827840e
Jeremy Galang <jjgalang@iastate.edu>
2020-10-13 16:50:46 -0500

    login still needs fixing

--- src/javascript/VerifyLogin.js
+++ src/javascript/VerifyLogin.js
@@ -13,9 +13,10 @@ function verify(){
   var mysql = require('mysql');
 
   var con = mysql.createConnection({
-    host: "coms-319-047.cs.iastate.edu",
-    user: "root",
-    password: "comsVM@319"
+    host: 'coms-319-047.cs.iastate.edu',
+    user: 'root',
+    password: 'comsVM@319',
+    database: 'project'
   });
 
   con.connect(function(err) {
