a72b9c82ef71580b027001028d940a5b31ce1360
Jeremy Galang <jjgalang@iastate.edu>
2020-10-18 20:22:10 -0500

    querying SQL works but frontend login doesn't work with it properly

--- src/javascript/VerifyLogin.js
+++ src/javascript/VerifyLogin.js
@@ -1,35 +1,44 @@
 // TODO THIS CLASS WILL CONTAIN MORE DATABASE LOGIC SOON
 
 function redirect(){
-  		//wait 2 seconds prior before going to the next page.
+
+	//wait 2 seconds prior before going to the next page.
         setTimeout(function(){
             window.location.href = 'inventoryManager.html';}
             , 2000);
 
 }
 
-// function verify(){
-//   var x = document.getElementById('first').value;
-//   console.log(x);
+function verify(){
 
-//   var mysql = require('mysql');
+var mysql = require('mysql');
 
-//   var con = mysql.createConnection({
-//     host: 'coms-319-047.cs.iastate.edu',
-//     user: 'team47',
-//     password: 'team47comsVM@319',
-//     database: 'MyProject',
+var connection = mysql.createConnection({
+    host: 'coms-319-047.cs.iastate.edu',
+     user: 'team47',
+     password: 'team47comsVM@319',
+    database: 'project',
     
-//   });
+});
 
-//   con.connect(function(err){
-//     if(!err) {
-//         console.log("Database is connected ... ");
-//     } else {
-//         console.log("Error connecting database ... ");
-//     }
-//   });
 
-// }
+ connection.connect(function(err){
 
-// verify();
+	 if (err) throw err;
+	  connection.query("SELECT * FROM user_info WHERE user='manager'", function (err, result, fields) {
+	    if (err) throw err;
+	    console.log(result);
+
+		console.log(user);
+		console.log(pass);
+
+	    console.log(result[0].user);
+	    console.log(result[0].password);
+	  });
+});
+
+
+
+
+}
+
--- src/javascript/startServer.js
+++ src/javascript/startServer.js
@@ -2,21 +2,31 @@
 
 //To start it, go in the command line and type in 'node startServer.js'
 
+function start(){
 
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
     host: 'coms-319-047.cs.iastate.edu',
-    user: 'team47',
-    password: 'team47comsVM@319',
-    database: 'MyProject',
+     user: 'team47',
+     password: 'team47comsVM@319',
+    database: 'project',
     
 });
 
-con.connect(function(err){
-    if(!err) {
-        console.log("Database is connected ... ");
-    } else {
-        console.log("Error connecting database ... ");
-    }
+connection.connect(function(err){
+
+ if (err) throw err;
+  connection.query("SELECT * FROM user_info WHERE user='manager'", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+
+    console.log(result[0].user);
+    console.log(result[0].password);
+    
+  });
 });
+
+}
+
+start();
