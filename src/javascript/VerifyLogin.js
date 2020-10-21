f9b3c30e07a1b32805f8c0ce2f61f0af25cfe60c
Jeremy Galang <jjgalang@iastate.edu>
2020-10-20 20:06:59 -0500

    login fix

--- src/javascript/VerifyLogin.js
+++ src/javascript/VerifyLogin.js
@@ -2,43 +2,50 @@
 
 function redirect(){
 
-	//wait 2 seconds prior before going to the next page.
+	var user = document.getElementById('first').value;
+	var pass = document.getElementById('pass').value;
+
+	if(user == "manager" && pass == "319sort"){
         setTimeout(function(){
             window.location.href = 'inventoryManager.html';}
             , 2000);
+	}
+	else{
+		alert("Incorrect username/password!");
+	}
 
 }
 
-function verify(){
+// function verify(){
 
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: 'coms-319-047.cs.iastate.edu',
-     user: 'team47',
-     password: 'team47comsVM@319',
-    database: 'project',
+// var connection = mysql.createConnection({
+//     host: 'coms-319-047.cs.iastate.edu',
+//      user: 'team47',
+//      password: 'team47comsVM@319',
+//     database: 'project',
     
-});
+// });
 
 
- connection.connect(function(err){
+//  connection.connect(function(err){
 
-	 if (err) throw err;
-	  connection.query("SELECT * FROM user_info WHERE user='manager'", function (err, result, fields) {
-	    if (err) throw err;
-	    console.log(result);
+// 	 if (err) throw err;
+// 	  connection.query("SELECT * FROM user_info WHERE user='manager'", function (err, result, fields) {
+// 	    if (err) throw err;
+// 	    console.log(result);
 
-		console.log(user);
-		console.log(pass);
+// 		console.log(user);
+// 		console.log(pass);
 
-	    console.log(result[0].user);
-	    console.log(result[0].password);
-	  });
-});
-
-
+// 	    console.log(result[0].user);
+// 	    console.log(result[0].password);
+// 	  });
+// });
 
 
-}
+
+
+//}
 
--- src/javascript/startServer.js
+++ src/javascript/startServer.js
@@ -1,10 +1,9 @@
-//This class starts up the SQL server
-
-//To start it, go in the command line and type in 'node startServer.js'
-
-function start(){
-
 var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
 
 var connection = mysql.createConnection({
     host: 'coms-319-047.cs.iastate.edu',
@@ -14,19 +13,53 @@ var connection = mysql.createConnection({
     
 });
 
-connection.connect(function(err){
+var app = express();
 
- if (err) throw err;
-  connection.query("SELECT * FROM user_info WHERE user='manager'", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
-    console.log(result[0].user);
-    console.log(result[0].password);
-    
-  });
+ app.get('/', function(request, response) {
+
+    response.sendFile(path.join(__dirname, '/../html/index.html'));
+  
+ });
+
+
+// for action
+app.post('/login', function(request, response) {
+    var username = request.body.first;
+    var password = request.body.pass;
+    if (username && password) {
+// check if user exists
+        connection.query("SELECT * FROM user_info WHERE user='?'", [username], function(error, results, fields) {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect('/home');
+            } else {
+                response.send('Incorrect Username and/or Password!');
+            }           
+            response.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
+    }
 });
 
-}
+app.get('/dashboard', function(request, response) {
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.username + '!');
+    } else {
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
 
-start();
+
+app.listen(3000);
