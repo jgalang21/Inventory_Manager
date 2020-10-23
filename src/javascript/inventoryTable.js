4126f4285e8f28b32fd392878040724fa8b7dbfe
Riley Thoma <rthoma@iastate.edu>
2020-10-23 08:52:35 -0500

    Added remove product by name functionality and cleaned up other code.

--- src/javascript/inventoryTable.js
+++ src/javascript/inventoryTable.js
@@ -35,47 +35,47 @@ class product {
 }
 
 function checkIfEmpty() {
-    var isEmpty = false,
+    var isEmpty = false;
 
-    name = document.getElementById("popupPrdName").value,
-    quantity = document.getElementById("popupQuantity").value,
-    cost = document.getElementById("popupCostPerItem").value,
-    weight = document.getElementById("popupWeightPerItem").value,
-    type = document.getElementById("popupProductType").value,
-    brand = document.getElementById("popupBrand").value,
-    id = document.getElementById("popupProductID").value,
-    location = document.getElementById("popupLocation").value;
+    var name = document.getElementById("addPopPrdName").value;
+    var quantity = document.getElementById("popupQuantity").value;
+    var cost = document.getElementById("popupCostPerItem").value;
+    var weight = document.getElementById("popupWeightPerItem").value;
+    var type = document.getElementById("popupProductType").value;
+    var brand = document.getElementById("popupBrand").value;
+    var id = document.getElementById("popupProductID").value;
+    var location = document.getElementById("popupLocation").value;
 
-    if(name == ""){
+    if(name == "") {
       alert("Name of the item can not be empty");
       isEmpty = true;
     }
-    else if(quantity == ""){
-      alert("Quantity of the item can not be empty")
+    else if(quantity == "") {
+      alert("Quantity of the item can not be empty");
       isEmpty = true;
     }
-    else if(cost == ""){
-      alert("Cost of the item can not be empty")
+    else if(cost == "") {
+      alert("Cost of the item can not be empty");
       isEmpty = true;
     }
-    else if(weight == ""){
-      alert("Weight of the item can not be empty")
+    else if(weight == "") {
+      alert("Weight of the item can not be empty");
       isEmpty = true;
     }
-    else if(type == ""){
-      alert("Type of the item can not be empty")
+    else if(type == "") {
+      alert("Type of the item can not be empty");
       isEmpty = true;
     }
-    else if(brand == ""){
-      alert("Brand of the item can not be empty")
+    else if(brand == "") {
+      alert("Brand of the item can not be empty");
       isEmpty = true;
     }
-    else if(id == ""){
-      alert("Id of the item can not be empty")
+    else if(id == "") {
+      alert("Id of the item can not be empty");
       isEmpty = true;
     }
-    else if(location == ""){
-      alert("Location of the item can not be empty")
+    else if(location == "") {
+      alert("Location of the item can not be empty");
       isEmpty = true;
     }
     return isEmpty;
@@ -89,11 +89,10 @@ function addProductWindow() {
 
     var newProduct = new product(1, 1, 1, 1, 1, 1, 1, 1);
 
-    var addProdPopupBtn = document.getElementsByClassName("popupBtn")[0];
+    var addProdPopupBtn = popup.getElementsByClassName("popupBtn")[0];
     addProdPopupBtn.onclick = function() {
 
-
-      newProduct.productName = document.getElementById("popupPrdName").value;
+      newProduct.productName = document.getElementById("addPopPrdName").value;
       newProduct.quantity = document.getElementById("popupQuantity").value;
       newProduct.costPerItem = document.getElementById("popupCostPerItem").value;
       newProduct.weightPerItem = document.getElementById("popupWeightPerItem").value;
@@ -104,28 +103,45 @@ function addProductWindow() {
 
       if(!checkIfEmpty()) {
         addRow(newProduct);
-
+        //this just simply clears the field after every item is added, just for making things clear
+        document.getElementById("addPopPrdName").value = '';
+        document.getElementById("popupQuantity").value ='';
+        document.getElementById("popupCostPerItem").value = '';
+        document.getElementById("popupWeightPerItem").value = '';
+        document.getElementById("popupProductType").value = '';
+        document.getElementById("popupBrand").value = '';
+        document.getElementById("popupProductID").value = '';
+        document.getElementById("popupLocation").value = '';
       }
-
-      //this just simply clears the field after every item is added, just for making things clear
-      document.getElementById("popupPrdName").value = '';
-      document.getElementById("popupQuantity").value ='';
-      document.getElementById("popupCostPerItem").value = '';
-      document.getElementById("popupWeightPerItem").value = '';
-      document.getElementById("popupProductType").value = '';
-      document.getElementById("popupBrand").value = '';
-      document.getElementById("popupProductID").value = '';
-      document.getElementById("popupLocation").value = '';
-
     }
 
     // Get the <span> element that closes the popup
-    var spanClose = document.getElementsByClassName("close")[0];
+    var spanClose = popup.getElementsByClassName("close")[0];
 
     // When the user clicks on <span> (x), close the popup
     spanClose.onclick = function() {
         popup.style.display = "none";
     }
+}
+
+function rmvProductWindow() {
+  //Get the popup
+  var popup = document.getElementById("myRmvProductPopup");
+  popup.style.display = "block";
+
+  var rmvProdPopupBtn = popup.getElementsByClassName("popupBtn")[0];
+  rmvProdPopupBtn.onclick = function() {
+    var prodName = document.getElementById("rmvPopPrdName").value;
+    removeRow(prodName);
+  }
+
+  // Get the <span> element that closes the popup
+  var spanClose = popup.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the popup
+  spanClose.onclick = function() {
+      popup.style.display = "none";
+  }
 }
 
 function addRow(newProduct) {
@@ -140,7 +156,25 @@ function addRow(newProduct) {
     popup.style.display = "none";
 }
 
-function removeRow() {
+function removeRow(prod) {
     var inventoryTable = document.getElementById('myInventoryTable').getElementsByTagName('tbody')[0];
-    inventoryTable.deleteRow(inventoryTable.rows.length - 1);
+
+    var prodFound = false;
+    for(var i = 0; i < inventoryTable.rows.length; i++) {
+      if(inventoryTable.rows[i].getElementsByTagName('td')[0].innerText == prod) {
+        prodFound = true;
+        inventoryTable.deleteRow(i);
+        document.getElementById("rmvPopPrdName").value = '';
+        break;
+      }
+    }
+
+    if(!prodFound) {
+      alert("Product not found, try again!");
+    }
+    else {
+      var popup = document.getElementById("myRmvProductPopup");
+      popup.style.display = "none";
+    }
+    
 }
