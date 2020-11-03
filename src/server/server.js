79c31f31d9af145a5ad8504c3cfdbd8eafab1327
Jeremy Galang <jjgalang@iastate.edu>
2020-11-03 15:06:24 -0600

    server works now

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const sql = require('mysql');
+const mysql = require('mysql');
 const bp = require('body-parser');
 const cors = require('cors');
 
 
 const app = express();
 const port = 8080;
+
+var connection = mysql.createConnection({
+   host: 'coms-319-047.cs.iastate.edu',
+    user: 'team47',
+    password: 'team47comsVM@319',
+   database: 'project',
+   
+});
+
 
 app.use(bp.urlencoded({extended : true}));
 app.use(bp.json());
@@ -22,10 +31,21 @@ app.get('/', function(request, response) {
     console.log(product);
     console.log(product.productName);
     response.status(200).json({"data": product});
-    
+
+    connection.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected!");
+      var sql = "INSERT INTO products VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+      connection.query(sql, [product.productName, product.quantity,
+      product.costPerItem, product.weightPerItem, product.productType,
+      product.brand, product.productID, product.location], function (err, result) {
+         if (err) throw err;
+         console.log("1 record inserted");
+  });
+    });
 
 
  });
 
- app.listen(port, () =>(console.log(`server running on port ${port}`)));
+ app.listen(port, () =>(console.log(`Server running on port ${port}`)));
  module.exports = app;
