690d13bd39b91129c746ea8e286eaa0411679e3b
Riley Thoma <rthoma@iastate.edu>
2020-11-19 11:57:53 -0600

    testing some on our app now

--- src/javascript/seleniumTests.js
+++ src/javascript/seleniumTests.js
@@ -4,10 +4,14 @@ const chromedriver = require('chromedriver');
 
 chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
+const pathLoginPage="file:///R:/rthomaHDD/Documents/ComS319/VSCworkspace/g47/src/html/index.html";
+
 //Open google and search for Selenium
 async function example() {
     let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("http://google.com");
-    await driver.findElement(By.name("q")).sendKeys("Selenium",Key.RETURN);
+    await driver.get(pathLoginPage);
+    await driver.findElement(By.name("first")).sendKeys("Test");
+    await driver.findElement(By.name("pass")).sendKeys("319sort");
+    await (await driver.findElement(By.name("continue"))).click();
 }
 example();
